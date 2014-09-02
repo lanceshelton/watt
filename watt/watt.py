2dd093e5daba39ffcd4f0059117f5d03b1745513
Lance Shelton <lshelton@air12.local>
2014-09-02 00:08:18 -0400

    Thread improvements
    Replaced the two racing threads - program (main) and input - with
    two command generating threads - program and input - and a command
    receiving thread (main).

--- watt/watt.py
+++ watt/watt.py
@@ -8,29 +8,32 @@ python ./watt.py -p arp
 
 Testing from the command line:
 watt = WattOutput(verbose=True)
-watt.write_cmd(command, watt.last_timestamp + 10, force=True)
+watt.write_cmd({'cmd': command, 'time': watt.last_timestamp + 10})
 watt.stop()
 """
 
 from optparse import OptionParser
 import pygame
 import pygame.midi
+from Queue import Queue
 import sys
 import termios
-import thread
+import threading
 from time import sleep
 import tty
 from api import *  # pylint: disable=unused-wildcard-import,wildcard-import
 from banks import *  # pylint: disable=unused-wildcard-import,wildcard-import
 
 TESTFILE = './watt.out'
-LOOP_BUFFER_MSECS = 3 * 1000
-LOOP_SLEEP_MSECS = 100
-NODEV_BUFFER_MSECS = 2 * LOOP_SLEEP_MSECS
+LOOP_BUFFER_SECS = .2
+LOOP_SLEEP_SECS = .1
+NODEV_BUFFER_SECS = 2 * LOOP_SLEEP_SECS
+
+# the buffer must be longer than the loop sleep or starvation will occur
+assert LOOP_BUFFER_SECS >= LOOP_SLEEP_SECS * 2
 
 # Controls
 BPM_OFFSET = 0
-WATT = None
 
 KEYBOARD_MAP = {
     'a': P1,
@@ -48,8 +51,9 @@ KEYBOARD_MAP = {
     'k': P8,
     }
 
-# the buffer must be longer than the loop sleep or starvation will occur
-assert LOOP_BUFFER_MSECS >= LOOP_SLEEP_MSECS * 2
+#
+# Device I/O
+#
 
 class WattOutput(pygame.midi.Output):
     """Manage the MIDI output.  There are currently some simplifications here
@@ -87,20 +91,14 @@ class WattOutput(pygame.midi.Output):
         """Wait until all scheduled commands have completed
         """
         while self.last_timestamp > pygame.midi.time():
-            sleep(1)
-        print "Waited until %s for last command at %s\r" % (
-            pygame.midi.time(), self.last_timestamp)
+            sleep(LOOP_SLEEP_SECS)
 
     def stop(self):
         """Stop the device
         """
-        self.write_cmd({'effect': Effect.diveBomb,
-                        'stomp': STOMP_ENABLE,
-                        'toe': MUTE}, self.last_timestamp + 10, force=True)
-        self.write_cmd({'effect': Effect.diveBomb,
-                        'stomp': STOMP_ENABLE,
-                        'toe': MUTE}, self.last_timestamp + 1000, force=True)
         self.wait_last()
+        # give the commands time to flush out to the device
+        sleep(3)
         if self.fdev is not None:
             self.fdev.close()
         if self.port != -1:
@@ -119,35 +117,38 @@ class WattOutput(pygame.midi.Output):
             self.write([[byte_array, timestamp]])
         self.update_last_timestamp(timestamp)
 
-    def write_cmd(self, command, timestamp, force=False):
+    def write_cmd(self, command):
         """Write out a command
         """
+        cmd = command['cmd']
+        timestamp = command['time']
+        force = True if 'force' in command and command['force'] else False
         delay = 0
-        if 'stomp' in command:
-            if command['stomp'] != self.stomp or force:
-                self.stomp = command['stomp']
-                self.write_out([0xb0, 0, command['stomp']], timestamp)
+        if 'stomp' in cmd:
+            if cmd['stomp'] != self.stomp or force:
+                self.stomp = cmd['stomp']
+                self.write_out([0xb0, 0, cmd['stomp']], timestamp)
                 #delay += 1
-        if 'effect' in command:
-            if command['effect'] != self.effect or force:
-                self.effect = effect = command['effect']
+        if 'effect' in cmd:
+            if cmd['effect'] != self.effect or force:
+                self.effect = effect = cmd['effect']
                 # Setting the effect also sets the stomp state.  Using patches
                 # 0-15 enables the pedal, using 16-31 maps to the same effects
                 # but bypassed.
-                if 'stomp' in command and command['stomp'] == STOMP_BYPASS:
+                if 'stomp' in cmd and cmd['stomp'] == STOMP_BYPASS:
                     effect = effect + 16
 
                 self.write_out([0xc0, effect], timestamp + delay)
                 #delay += 1
-        if 'toe' in command:
-            if type(command['toe']) is str:
-                if command['toe'] not in INTERVAL_MAP[self.effect]:
+        if 'toe' in cmd:
+            if type(cmd['toe']) is str:
+                if cmd['toe'] not in INTERVAL_MAP[self.effect]:
                     print 'not in interval map\r'
                     toe = self.toe
                 else:
-                    toe = INTERVAL_MAP[self.effect][command['toe']]
+                    toe = INTERVAL_MAP[self.effect][cmd['toe']]
             else:
-                toe = command['toe']
+                toe = cmd['toe']
             if toe != self.toe or force:
                 self.write_out([0xb0, 11, toe], timestamp + delay)
                 self.toe = toe
@@ -159,76 +160,148 @@ class WattOutput(pygame.midi.Output):
         ms_per_beat = 60 * 1000 / bpm
         return int((beats * measure + beat) * ms_per_beat)
 
-    def write_program(self, start_time, prog):
-        """Write out a program
-        """
-        bpm = prog.bpm
-        beats = prog.beats
-        cur_bar = -1
-        for command in prog.commands:
-            if self.verbose:
-                if command['bar'] > cur_bar:
-                    print 'start of bar %s\r' % command['bar']
-                cur_bar = command['bar']
-            tstamp = self.beat_to_ts(bpm, beats, command['bar'],
-                                     command['beat'])
-            self.write_cmd(command, start_time + tstamp)
+#
+# Thread helpers
+#
 
-def wait_for_input():
-    """Get a single character of input, validate
+def write_program(watt, cmd_q, start_time, prog):
+    """Queue a program
     """
-    global BPM_OFFSET  # pylint: disable=global-statement
-    try:
-        while True:
-            key = sys.stdin.read(1)
-            if key in '-_':
-                BPM_OFFSET -= 10
-            elif key in '=+':
-                BPM_OFFSET += 10
-            elif key in KEYBOARD_MAP:
-                WATT.write_cmd({'toe': KEYBOARD_MAP[key]},
-                               WATT.last_timestamp + 10)
-            elif key not in 'qwertyuiopasdfghjklzxcvbnm':
-                print 'received key ' + key + ': exiting\r'
-                # exit immediately on unmapped key
-                thread.interrupt_main()
-                break
-    # failing to catch an exception here causes the terminal to hang, hence
-    # the bare except
-    except:  # pylint: disable=bare-except
-        print 'Exception in input handler', sys.exc_info()[0]
-        thread.interrupt_main()
+    bpm = prog.bpm
+    beats = prog.beats
+    cur_bar = -1
+    for command in prog.commands:
+        if watt.verbose:
+            if command['bar'] > cur_bar:
+                print 'start of bar %s\r' % command['bar']
+            cur_bar = command['bar']
+        tstamp = watt.beat_to_ts(bpm, beats, command['bar'], command['beat'])
+        cmd_q.put({'cmd': command, 'time': start_time + tstamp})
 
-def play_loop(watt, prog, count):
-    """Queue commands
+#
+# Threads
+#
+
+def program_thread(watt, cmd_q, stop_event, prog, count):
+    """Generate commands from a program and push them onto the queue
     """
     # do not queue up much ahead of time without a real device with real latency
     if watt.port == -1:
-        buffer_msecs = NODEV_BUFFER_MSECS
+        buffer_secs = NODEV_BUFFER_SECS
     else:
-        buffer_msecs = LOOP_BUFFER_MSECS
+        buffer_secs = LOOP_BUFFER_SECS
 
     default_bpm = prog.bpm
 
     # Need some time to let initialization complete
-    consumed_time = pygame.midi.time() + buffer_msecs
+    consumed_time = pygame.midi.time() + buffer_secs * 1000
 
     # count == -1 for infinite play
     while count != 0:
         # sleep when the buffer is full
-        while consumed_time > pygame.midi.time() + buffer_msecs:
-            sleep(.1)
+        while consumed_time > pygame.midi.time() + buffer_secs * 1000:
+            sleep(buffer_secs)
+            if stop_event.is_set():
+                break
+        if stop_event.is_set():
+            break
+
         prog.bpm = max(1, default_bpm + BPM_OFFSET)
-        watt.write_program(consumed_time, prog)
+        write_program(watt, cmd_q, consumed_time, prog)
         consumed_time += watt.beat_to_ts(prog.bpm, prog.beats, prog.measures, 0)
         if count > 0:
             count -= 1
 
+def input_thread(watt, cmd_q):
+    """Get characters from stdin, push the resulting commands onto the queue
+    """
+    global BPM_OFFSET  # pylint: disable=global-statement
+    while True:
+        key = sys.stdin.read(1)
+        if key in '-_':
+            BPM_OFFSET -= 10
+        elif key in '=+':
+            BPM_OFFSET += 10
+        elif key in KEYBOARD_MAP:
+            cmd_q.put({'cmd': {'toe': KEYBOARD_MAP[key]},
+                       'time': watt.last_timestamp + 10})
+        # any non-alphabet character exits
+        elif key not in 'qwertyuiopasdfghjklzxcvbnm':
+            print 'received key ' + key + ': exiting\r'
+            # exit immediately on unmapped key
+            break
+
+def command_thread(watt, cmd_q, stop_event):
+    """Pop commands from the queue and write them out
+    """
+    while True:
+        # wait for a command
+        command = cmd_q.get()
+        # write out the command
+        watt.write_cmd(command)
+        if stop_event.is_set():
+            break
+
+def run_threads(watt, programs, program, count):
+    """Initialize queue, run threads
+    """
+    cmd_q = Queue()
+    program_stop_event = threading.Event()
+    command_stop_event = threading.Event()
+
+    # command thread
+    c_thread = threading.Thread(target=command_thread,
+                                args=(watt, cmd_q, command_stop_event))
+    c_thread.start()
+
+    # program thread
+    if program is not None:
+        p_thread = threading.Thread(target=program_thread,
+                                    args=(watt, cmd_q, program_stop_event,
+                                          programs[program](),
+                                          int(count)))
+        p_thread.start()
+    else:
+        # initialize to upOctave if no program is specified.  This effect works
+        # well with live keyboard input
+        cmd_q.put({'cmd': {'effect': Effect.upOctave,
+                          'stomp': STOMP_ENABLE,
+                          'toe': P1},
+                   'time': watt.last_timestamp + 10})
+
+    # use the main thread for the input thread
+    input_thread(watt, cmd_q)
+
+    # signal program generation to stop when the input thread exits
+    if program is not None:
+        program_stop_event.set()
+
+        # wait for the last program loop to finish (so that the next MUTE
+        # command will be the last scheduled command
+        watt.wait_last()
+        p_thread.join()
+
+    # signal the command thread to stop after the program has completed
+    command_stop_event.set()
+
+    # This is a little odd.  The command thread does not detect the stop
+    # event because it is waiting on an event.  We need to give it one last
+    # command to mute the device anyway, so this also serves the purpose of
+    # waking the command thread so that it will detect the stop event.
+    cmd_q.put({'cmd': {'effect': Effect.diveBomb,
+                       'stomp': STOMP_ENABLE,
+                       'toe': MUTE},
+               'time': watt.last_timestamp + 10})
+
+    c_thread.join()
+
+#
+# Setup
+#
+
 def main(args):
     """Parse arguments, set up terminal, call main loop
     """
-    global WATT  # pylint: disable=global-statement
-
     parser = OptionParser(description=__doc__)
     parser.add_option("-c", "--count", default='-1',
                       help="iterations to run")
@@ -248,6 +321,8 @@ def main(args):
         print 'Program %s not found in path' % options.program
         return -1
 
+    watt = WattOutput(verbose=options.verbose)
+
     # Set the terminal to unbuffered, to catch a single keypress
     infd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(infd)
@@ -255,24 +330,14 @@ def main(args):
         # change TTY settings (reverting them if an exception occurs)
         tty.setraw(infd)
 
-        # input thread
-        thread.start_new_thread(wait_for_input, tuple())
-
-        WATT = WattOutput(verbose=options.verbose)
-        if options.program is not None:
-            play_loop(WATT, programs[options.program](), int(options.count))
-        else:
-            WATT.write_cmd({'effect': Effect.upOctave,
-                            'stomp': STOMP_ENABLE,
-                            'toe': P1}, WATT.last_timestamp + 10)
-            while True:
-                sleep(1)
+        # main thread execution
+        run_threads(watt, programs, options.program, options.count)
     except (KeyboardInterrupt, SystemExit):
         # return term to normal state before exception is displayed
         termios.tcsetattr(infd, termios.TCSADRAIN, old_settings)
     finally:
-        if WATT:
-            WATT.stop()
+        if watt:
+            watt.stop()
         # always return term to normal state
         termios.tcsetattr(infd, termios.TCSADRAIN, old_settings)
 
